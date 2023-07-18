be76a8df94f8bf7087a2eff7174d09fc2d6e5fc1
muhamad.indrawan94 <muhamad.indrawan94@gmail.com>
2023-07-18 10:11:45 +0700

    upload projek vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <header></header> -->
+    <HeaderShayna/>
+    <OwlCarousel/>
+    <WomanShayna/>
+    <InstaShayna/>
+    <PartnerLogoShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import OwlCarousel from '@/components/OwlCarousel.vue';
+import WomanShayna from '@/components/WomanShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerLogoShayna from '@/components/PartnerLogoShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+// import carousel from 'vue-owl-carousel';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    OwlCarousel,
+    WomanShayna,
+    InstaShayna,
+    PartnerLogoShayna,
+    FooterShayna,
+  }
+}
+</script>
